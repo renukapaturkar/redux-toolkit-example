1a45bb12bee0213d63b4ba68f8ee7e6d94782110
Renuka Paturkar <renukapaturkar@emertech>
2022-01-14 17:47:11 +0530

    prettify code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './app/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./app/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
-    <App />
-    </Provider>
+        <App />
+      </Provider>
     </Router>
-    
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
